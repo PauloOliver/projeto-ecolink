cb2e56aba2bcb52d6ed273ae05813a5e35677164
João Gabriel Abbado De Souza <joaoabbado@hotmail.com>
2025-08-30 20:03:18 -0300

    teste branch

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
         <nav className="nav">
           <a href="#about">Sobre</a>
-          <a href="#projects">Projetos</a>
+          <a href="#projects">joao gabriel</a>
           <a href="#contact">Contato</a>
         </nav>
       </header>
