32794035edd15e7e43ec897616d7ec04457fe082
PauloOliver <63667729+PauloOliver@users.noreply.github.com>
2025-09-08 23:43:40 -0300

    Navbar - v1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { NavigationBar } from './components/Navbar.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { NavigationBar } from "./components/Navbar";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <NavigationBar></NavigationBar>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode> 
+    <NavigationBar />
+  </StrictMode>
+);
