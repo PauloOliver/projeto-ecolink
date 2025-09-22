a324f5fa97106a93619f32054d9b4a809f2e74a5
PauloOliver <63667729+PauloOliver@users.noreply.github.com>
2025-09-22 00:08:35 -0300

    Imagens de teste e melhoria no cardcomponent.
    Agora é possivel passar uma imagem e um titulo para cada card

--- frontend/src/components/CarouselComponent.tsx
+++ frontend/src/components/CarouselComponent.tsx
@@ -1,4 +1,7 @@
 import { Carousel } from "flowbite-react";
+import img1 from '../assets/coleta_lixo_praia.jpg'
+import img2 from '../assets/garrafas.jpg'
+import img3 from '../assets/separar_caixa.jpg'
 
 const customTheme = {
   scrollContainer: {
@@ -9,13 +12,11 @@ const customTheme = {
 
 export function CarouselComponent() {
   return (
-    <div className="m-6 px-4 h-56 sm:h-64 xl:h-80 2xl:h-96">
+    <div className="m-6 px-4 h-[80vh] w-[90vw] justify-center">
       <Carousel slideInterval={800} theme={customTheme}>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <img src={img1} alt="..." />
+        <img src={img2} alt="..." />
+        <img src={img3} alt="..." />
       </Carousel>
     </div>
   );
